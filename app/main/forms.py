f28b012a18ba1c0242e20f41cf71d6f2b192fa1d
vanshin <kfx721@hotmail.com>
2016-11-02 13:53:30 +0800

    数据库模型(0.1)

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@ from wtforms import TextAreaField,SubmitField,StringField
 from wtforms.validators import Required,Length
 
 class readingForm(Form):
-    title = StringField('标题',validators=[Required()])
-    reading = TextAreaField('阅读理解')
+    order_name = StringField('序列',validators=[Required()])
+    reading_name = StringField('标题',validators=[Required()])
+    reading_body = TextAreaField('阅读理解')
     submit = SubmitField('提交')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from . import main
 from flask import render_template
 from .forms import readingForm
-
+from ..models import Reading,Sentence,Word
+from .. import db
 
 @main.route('/')
 def index():
@@ -11,5 +12,14 @@ def index():
 def input():
     form = readingForm()
     if form.validate_on_submit():
-        print('111')
+        order_name = form.order_name
+        reading_name = form.reading_name
+        reading_body = form.reading_body
+        sentences_list = reading_body.split('.')
+        reading = Reading(order_name=order_name,reading_name=reading_name)
+        reading_id = reading.id
+        db.session.add(reading)
+        for sentence_body in sentences_list:
+            sentence = (reading_id=reading_id,sentence_body=sentence_body)
+            db.session.add(sentence)
     return render_template('input.html',form=form)

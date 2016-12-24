e5df9746256c20ab274223ec92179fc9110d197c
vanshin-ubuntu <kfx721@hotmail.com>
2016-12-25 01:27:40 +0800

    完善

--- app/static/input.js
+++ app/static/input.js
@@ -1,11 +1,23 @@
+button = $('#submitreading')
+
+function clearForm(){
+	var reading = $("textarea#reading")
+		order = $("input#order")
+		rname = $("input#rname")
+	reading.val("")
+	order.val("")
+	rname.val("")
+}
+
 $("#submitreading").click(function(){
 	var reading = $("textarea#reading").val()
 		order = $("input#order").val()
-		name = $("input#name").val()
-
+		rname = $("input#rname").val()
+		// button = $('#submitreading')
+		button.attr({"disabled":"disabled"})
 	json_reading = {
 		"reading_order": order,
-		"reading_name": name,
+		"reading_name": rname,
 		"reading_body": reading
 	}
 	$.ajax({
@@ -15,7 +27,8 @@ $("#submitreading").click(function(){
 		dataType: "json",
 		contentType: "application/json"
 	}).done(function(data){
+		clearForm()
+		button.removeAttr("disabled")
 		
-		console.log("post success")
 	})
 })
